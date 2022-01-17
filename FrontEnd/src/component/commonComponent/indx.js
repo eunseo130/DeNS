dfcb8fce5ed12452d42bb2eaf848de384463b68b
seungkyu-lee <leesk3732@naver.com>
2022-01-17 18:13:35 +0900

    import subpjt1

---
+++ FrontEnd/src/component/commonComponent/indx.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Indx(props) {
+  return <Container></Container>
+}
+
+const Container = styled.div`
+  background-color: #e6e6e6;
+  width: 620px;
+  height: 818px;
+`
+
+export default Indx
