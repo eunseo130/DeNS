de0d58ee10d6e8cc2c2316f14d9521cf6884bc0f
윤설 <computer98400@gmail.com>
2022-01-17 16:38:31 +0900

    modify featuretest2

--- FrontEnd/src/component/commonComponent/test2.js
+++ FrontEnd/src/component/commonComponent/test2.js
@@ -1,5 +1,4 @@
-console.log("testtttttttttt");
-console.log("testtttttttttt");
+console.log("testtttttttttttesttttttttttt");
 console.log("testtttttttttt");
 console.log("testtttttttttt");
 console.log("testtttttttttt");
