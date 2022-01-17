6a6b65f9513b84d11ea849e4fc451bbea3e26e58
윤설 <computer98400@gmail.com>
2022-01-17 16:29:44 +0900

    modify testjs

--- FrontEnd/src/component/commonComponent/test.js
+++ FrontEnd/src/component/commonComponent/test.js
@@ -2,5 +2,11 @@ console.log("Test");
 function add(x, y) {
     console.log(x);
     console.log(y);
+    console.log(y);
+    console.log(y);
+    console.log(y);
+    console.log(y);
+    console.log(y);
+
     console.log(x+y);
 }
