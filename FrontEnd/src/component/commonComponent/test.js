1bbd521436c7aeaa5112877b8715ce8523f483e6
윤설 <computer98400@gmail.com>
2022-01-17 16:36:28 +0900

    modify testjs2

--- FrontEnd/src/component/commonComponent/test.js
+++ FrontEnd/src/component/commonComponent/test.js
@@ -6,6 +6,17 @@ function add(x, y) {
     console.log(x);
     console.log(x);
     console.log(x);
+    console.log(x);
+    console.log('xasdfasdf');
+    console.log('xasdfasdf');
+    console.log('xasdfasdf');
+    console.log('xasdfasdf');
+    console.log('xasdfasdf');
+    console.log(x);
+    console.log(x);
+    console.log(x);
+    console.log(x);
+    console.log(x);
     console.log(y);
     console.log(y);
     console.log(y);
