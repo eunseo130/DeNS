bb1202258b69470fd575f2f9b3ebd2846a9aefa1
seungkyu-lee <leesk3732@naver.com>
2022-01-20 11:10:14 +0900

    feature_lee_유효성

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import { Route, Link, Router, Switch } from 'react-router-dom';
-import SignupBox from './component/signupComponent/SignupBox';
-import Login from './component/loginComponent/Login';
-import BeforeLogin from './component/commonComponent/BeforeLogin';
+import React from 'react'
+import { Route, Link, Router, Switch } from 'react-router-dom'
+import SignupBox from './component/signupComponent/SignupBox'
+import Login from './component/loginComponent/Login'
+import BeforeLogin from './component/commonComponent/BeforeLogin'
 
 const App = () => {
-    return (
-        <>
-            <Route path="/" exact={true } >
-                <BeforeLogin /> 
-            </Route>
-            <Route path="/signup" >
-                <SignupBox />
-            </Route>
-            <Route path="/login">
+  return (
+    <>
+      {/* <Route path="/" exact={true } >
+                <BeforeLogin />
+            </Route> */}
+      <SignupBox />
+
+      {/* <Route path="/login">
                 <Login />
-            </Route>
-        </>
-    )
+            </Route> */}
+    </>
+  )
 }
-export default App;
+export default App
--- FrontEnd/src/component/commonComponent/BeforeLogin.js
+++ FrontEnd/src/component/commonComponent/BeforeLogin.js
@@ -2,10 +2,12 @@ import React, { Component, useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
 
 
+
 function BeforeLogin() {
   return (
     <>
       <span>testSyntex</span>
+
     </>
     )
 }
