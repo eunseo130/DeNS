702bd99a5b44e0ed80d8f6c265ece5a39fcea437
JunWoo Kim <k1mjunooo@naver.com>
2022-01-20 15:06:04 +0900

    feature_JW_add BackEnd folder

---
+++ BackEnd/src/main/java/com/ssafy/BackEnd/controller/UserController.java
@@ -0,0 +1,195 @@
+package com.ssafy.BackEnd.controller;
+
+import com.ssafy.BackEnd.entity.Request.RequestChangePassword1;
+import com.ssafy.BackEnd.entity.Request.RequestChangePassword2;
+import com.ssafy.BackEnd.entity.Request.RequestVerifyEmail;
+import com.ssafy.BackEnd.entity.Response;
+import com.ssafy.BackEnd.entity.User;
+
+import com.ssafy.BackEnd.service.AuthService;
+//import com.ssafy.TeamZOI.service.UserService;
+import com.ssafy.BackEnd.service.JwtServiceImpl;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor // final
+public class UserController {
+
+//    private final UserService userService;
+    private final JwtServiceImpl jwtService;
+    private final AuthService authService;
+
+//    @GetMapping("/#")
+//    public void Home() {
+//
+//    }
+//
+//    @GetMapping("/join")
+//    public void join(){
+//
+//    }
+
+    @PostMapping("/signup")
+    public Response signUp(User user) {
+        Response response = new Response();
+        System.out.println("up : "+user.getEmail());
+        try {
+            authService.signUp(user);
+            response.setResponse("success");
+            response.setMessage("회원가입을 성공적으로 완료했습니다.");
+            response.setData(null);
+        }
+        catch(Exception e) {
+            response.setResponse("failed");
+            response.setMessage("회원가입을 하는 도중 오류가 발생했습니다.");
+            response.setData(e.toString());
+        }
+        return response;
+    }
+
+    @GetMapping("/password/{key}")
+    public Response isPasswordUUIDValidate(@PathVariable String key) {
+        Response response = new Response();
+        try {
+            if (authService.isPasswordUuidValidate(key)) {
+                response.setResponse("success");
+                response.setMessage("정상적인 접근입니다.");
+                response.setData(null);
+            }
+            else {
+                response.setResponse("error");
+                response.setMessage("유효하지 않은 key값입니다.");
+                response.setData(null);
+            }
+        } catch (Exception e) {
+            response.setResponse("error");
+            response.setMessage("유효하지 않은 key값입니다.");
+            response.setData(null);
+        }
+        return response;
+    }
+
+    @PostMapping(value = "/password")
+    public Response requestChangePassword(RequestChangePassword1 requestChangePassword1) {
+        Response response = new Response();
+        try {
+            User user = authService.findByEmail(requestChangePassword1.getEmail());
+            if (!user.getEmail().equals(requestChangePassword1.getEmail())) throw new NoSuchFieldException("");
+            authService.requestChangePassword(user);
+            response.setResponse("success");
+            response.setMessage("성공적으로 사용자의 비밀번호를 변경요청을 수행했습니다.");
+            response.setData(null);
+        } catch (NoSuchFieldException e) {
+            response.setResponse("error");
+            response.setMessage("사용자의 정보를 조회할 수 없습니다.");
+            response.setData(null);
+        } catch (Exception e) {
+            response.setResponse("error");
+            response.setMessage("비밀번호 변경 요청을 할 수 없습니다.");
+            response.setData(null);
+        }
+        return response;
+    }
+
+    @PutMapping("/password")
+    public Response changePassword(RequestChangePassword2 requestChangePassword2) {
+        Response response = new Response();
+        System.out.println("hi");
+        System.out.println("hello");
+        System.out.println("hihi");
+        System.out.println("junu");
+
+        HttpStatus status;
+        try {
+            User user = authService.findByEmail(requestChangePassword2.getEmail());
+            authService.changePassword(user, requestChangePassword2.getPassword());
+            response.setResponse("success");
+            response.setMessage("사용자의 비밀번호를 성공적으로 변경했습니다.");
+            response.setData(null);
+        } catch (Exception e) {
+            response.setResponse("error");
+            response.setMessage("사용자의 비밀번호를 변경할 수 없습니다.");
+            response.setData(null);
+        }
+        return response;
+    }
+
+
+    @GetMapping("/signin")
+    public void signinPage() {
+        System.out.println("sign in hi");
+    }
+
+    @PostMapping("/signin") // 스켈레톤이랑 연결할땐 signin 지우고 '/' 상태에서
+    public ResponseEntity<Map<String, Object>> signin(User users,
+                                                      HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status;
+        System.out.println(users.getEmail()+" "+users.getPassword());
+        try {
+            final User user = authService.signIn(users.getEmail(), users.getPassword());
+            //System.out.println(user.getEmail()+" "+user.getPassword());
+            //System.out.println("user null ? "+user);
+            if(user != null) {
+                //System.out.println("1pass");
+                String token = jwtService.create("email", user.getEmail(), "access-token");
+                System.out.println("tk : "+token);
+                resultMap.put("access-token", token);
+                resultMap.put("message", "success");
+                status = HttpStatus.ACCEPTED;
+            } else {
+                //System.out.println("error");
+                resultMap.put("message", "fail");
+                status = HttpStatus.ACCEPTED;
+            }
+        } catch (Exception e) {
+            resultMap.put("message", e.getMessage());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+    @PostMapping("/verify")
+    public Response verify(RequestVerifyEmail requestVerifyEmail, HttpServletRequest req, HttpServletResponse res) {
+        Response response = new Response();
+        System.out.println("ve : "+requestVerifyEmail.getName());
+        try {
+            User user = authService.findByName(requestVerifyEmail.getName());
+            System.out.println("u : "+user.getName());
+            authService.sendVerificationMail(user);
+
+            response.setResponse("success");
+            response.setMessage("성공적으로 인증메일을 보냈습니다");
+            response.setData(null);
+        } catch (Exception exception) {
+            //response = new Response("error", "인증메일을 보내는데 문제가 발생했습니다.", exception);
+            response.setResponse("error");
+            response.setMessage("인증메일을 보내는데 문제가 발생했습니다");
+            response.setData(exception);
+        }
+        return response;
+    }
+
+    @GetMapping("/verify/{key}")
+    public Response getVerify(@PathVariable String key) {
+        Response response;
+        try {
+            authService.verifyEmail(key);
+            response = new Response("success", "성공적으로 인증메일을 확인했습니다.", null);
+
+        } catch (Exception e) {
+            response = new Response("error", "인증메일을 확인하는데 실패했습니다.", null);
+        }
+        return response;
+    }
+}
---
+++ BackEnd/src/main/java/com/ssafy/BackEnd/service/AuthServiceImpl.java
@@ -0,0 +1,126 @@
+package com.ssafy.BackEnd.service;
+
+import com.ssafy.BackEnd.entity.Salt;
+import com.ssafy.BackEnd.entity.User;
+import com.ssafy.BackEnd.entity.UserIdentity;
+import com.ssafy.BackEnd.repository.UserRepository;
+import javassist.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService{
+
+    @Autowired
+    private final EmailService emailService;
+
+    @Autowired
+    private final RedisUtil redisUtil;
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    @Autowired
+    private final SaltUtil saltUtil;
+    private String key;
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void signUp(User user) {
+        String password = user.getPassword();
+        String salt = saltUtil.genSalt();
+        user.setSalt(new Salt(salt));
+        user.setPassword(saltUtil.encodePassword(salt, password));
+        validateDuplicateUser(user);
+        userRepository.save(user);
+    }
+
+    private void validateDuplicateUser(User user) {
+        List<User> findUsers = userRepository.findByEmail(user.getEmail());
+        if (!findUsers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    @Override
+    public User signIn(String email, String password) throws Exception{
+        List<User> findUsers = userRepository.findByEmail(email);
+        User findUser = findUsers.get(0);
+        System.out.println("find "+findUser.getEmail());
+        if(findUsers.isEmpty()) throw new Exception ("멤버가 조회되지 않습니다.");
+        String salt = findUser.getSalt().getSalt();
+        password = saltUtil.encodePassword(salt, password);
+        System.out.println("pass : "+password);
+        System.out.println("u pass : "+findUser.getPassword());
+        if(!findUser.getPassword().equals(password))
+            throw new Exception("비밀번호가 틀립니다.");
+        return findUser;
+    }
+
+    @Override
+    public User findByEmail(String email) throws NotFoundException {
+        List<User> findUser = userRepository.findByEmail(email);
+        if(findUser.isEmpty()) throw new NotFoundException("멤버가 조회되지 않습니다.");
+        return findUser.get(0);
+    }
+
+    @Override
+    public User findByName(String name) throws NotFoundException {
+        User user = userRepository.findByName(name);
+        if(user == null) throw new NotFoundException("멤버가 조회되지 않습니다.");
+        return user;
+    }
+
+    @Override
+    public void requestChangePassword(User user) throws NotFoundException {
+        String CHANGE_PASSWORD_LINK = "http://localhost:8080/password/";
+        if(user == null) throw new NotFoundException("멤버가 조회되지 않습니다.");
+        String key = REDIS_CHANGE_PASSWORD_PREFIX+ UUID.randomUUID();
+        redisUtil.setDataExpire(key, user.getName(), 60*30L);
+        emailService.sendMail(user.getEmail(), "사용자 비밀번호 안내 메일", CHANGE_PASSWORD_LINK+key);
+    }
+
+    @Override
+    public boolean isPasswordUuidValidate(String key) {
+        String userId = redisUtil.getData(key);
+        return !userId.equals("");
+    }
+
+    @Override
+    public void changePassword(User user, String password) throws NotFoundException {
+        if(user==null) throw new NotFoundException("changePassword(), 멤버가 조회되지 않습니다.");
+        String salt = saltUtil.genSalt();
+        user.setSalt(new Salt(salt));
+        user.setPassword(saltUtil.encodePassword(salt, password));
+        userRepository.save(user);
+
+    }
+
+    public void sendVerificationMail(User user) throws NotFoundException {
+        String VERIFICATION_LINK = "http://localhost:8080/verify/";
+        if(user==null) throw new NotFoundException("멤버가 조회되지 않음");
+        UUID uuid = UUID.randomUUID();
+        System.out.println("key : " + uuid);
+        redisUtil.setDataExpire(uuid.toString(),user.getName(), 60 * 30L);
+        emailService.sendMail(user.getEmail(),"[팀 조이] 회원가입 인증메일입니다.",VERIFICATION_LINK+uuid.toString());
+    }
+
+    public void verifyEmail(String key) throws NotFoundException {
+        String userId = redisUtil.getData(key);
+        User user = userRepository.findByName(userId);
+        if(user==null) throw new NotFoundException("멤버가 조회되지않음");
+        modifyUserRole(user, UserIdentity.USER); //enum으로 수정
+        redisUtil.deleteData(key);
+    }
+
+    public void modifyUserRole(User user, UserIdentity role){
+        user.setIdentity(role);
+        userRepository.save(user);
+    }
+}
