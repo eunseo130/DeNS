d72ea53cc483fb1556a9eac4e5168e3aa0b57b60
KimHyeongJun <hjthelegend@naver.com>
2022-01-21 17:22:56 +0900

    feature_hyeongjun Login

--- FrontEnd/src/component/loginComponent/Login.js
+++ FrontEnd/src/component/loginComponent/Login.js
@@ -1,17 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
 import styled from "styled-components";
-import Btn from "../commonComponent/Btn";
-import InputBox from "../commonComponent/InputBox";
 
 function Login() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const onChangeId = (e) => {
+      setName(e.target.value);
+    }
+    const onChangePw = (e) => {
+      setPassword(e.target.value)
+    }
+    const LoginConsole = () => {
+      console.log([name, password])
+    };
     return (
       <LoginBox>
         <H3>로그인</H3>
-        <InputBox name="이름" ph="이름을 입력해주세요." />
-        <InputBox name="비밀번호" ph="비밀번호를 입력해주세요." />
-        <Btn name="로그인" style={LoginSqaure}/>
-      </LoginBox >
+        <Container>
+          <Name>이름</Name>
+          {/* <input placeholder="이름을 입력해주세요." onChange={(e) => Setname(e.target.value)}/>      */}
+          <input placeholder="이름을 입력해주세요." onChange={onChangeId} value={name}/>     
+        </Container>
+
+        <Container>
+          <Name>비밀번호</Name>
+          <input placeholder="비밀번호" onChange={onChangePw} value={password}/>     
+        </Container>
+        <Btn onClick={LoginConsole}>로그인</Btn>
+
+      </LoginBox>
     );
 }
 
@@ -29,9 +46,45 @@ const H3 = styled.h3`
   color: #F46A72; 
 `;
 
-const LoginSqaure = styled.button`
-  display: flex;
-  justify-content: center;
+const Btn = styled.button`
+  width:140px;
 `;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+`;
+
+const Name = styled.span`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 700;
+  color: rgba(131,131,131,1);
+  width: 150px;
+  height: 18px;
+  margin-left: 4px;
+`;
+
+const InputSquare = {
+  fontFamily: "Roboto",
+  fontStyle: "normal",
+  fontWeight: 700,
+  color: 'rgba(220,220,220,1)',
+  // width: '233px',
+  // height: '18px',
+  marginTop: '5px',
+  marginLeft: '10px',
+
+  width: '374px',
+  height: '36px',
+  backgroundColor: 'rgba(255,255,255,1)',
+  borderWidth: '1px',
+  borderColor: 'rgba(220,220,220,1)',
+  display: 'flex',
+  flexDirection: 'column',
+  borderStyle: 'solid',
+};
+
 
 export default Login;
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App'
+import Login from './component/loginComponent/Login'
+
+// ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+
+ReactDOM.render(<Login/>, document.getElementById('root'));
