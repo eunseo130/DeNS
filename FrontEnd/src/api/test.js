09a956d27a68c944af7fa66ddaf906b007b82936
seungkyu-lee <leesk3732@naver.com>
2022-02-03 17:38:37 +0900

    featuer_lee_profile

--- FrontEnd/src/api/test.js
+++ FrontEnd/src/api/test.js
@@ -1,25 +1,27 @@
-import { apiInstance } from './index.js';
+import { apiInstance } from './index.js'
 
-const api = apiInstance();
+const api = apiInstance()
 //localhost:8080/test22 -> 를 가져오고싶다.
 function test22(param, success, fail) {
-    api.get(`/test22`,param).then(success).catch(fail);
+  api.get(`/test22`, param).then(success).catch(fail)
 }
 
 function signup(param, success, fail) {
-    api.post(`/signup`,param).then(success).catch(fail);
+  api.post(`/signup`, param).then(success).catch(fail)
 }
 
 function login(param, success, fail) {
-    api.post(`/signin`,JSON.stringify(param)).then(success).catch(fail);
+  api.post(`/signin`, JSON.stringify(param)).then(success).catch(fail)
 }
 
 function profileTest(param, success, fail) {
+  console.log('profile get')
   api.get(`/profile/${param}`).then(success).catch(fail)
 }
-function profileUpdate({ url, data }, success, fail) {
+function profileUpdate({ id, param }, success, fail) {
   console.log('profile update')
-  api.put(`/profile/${url}`, JSON.stringify(data)).then(success).catch(fail)
+  console.log(param)
+  api.put(`/profile/${id}`,param).then(success).catch(fail)
 }
 function dummytest(param, success, fail) {
   console.log('check keyword' + param)
