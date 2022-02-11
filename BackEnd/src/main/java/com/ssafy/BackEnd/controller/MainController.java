bc767ed29055103047019fe7b57d798e53edb030
JunWoo Kim <k1mjunooo@naver.com>
2022-02-11 23:38:45 +0900

    add test11 method

--- BackEnd/src/main/java/com/ssafy/BackEnd/controller/MainController.java
+++ BackEnd/src/main/java/com/ssafy/BackEnd/controller/MainController.java
@@ -48,6 +48,17 @@ public class MainController {
     private final ProfileService profileService;
 
 
+    @GetMapping("/test11")
+    public ResponseEntity<Map<String, Object>> test11(){
+        logger.info("test11");
+        System.out.println("teset11이에요");
+        Map<String, Object> map = new HashMap<>();
+        map.put("message", "test11");
+        map.put("success", "성공");
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
 
     @PostMapping("/signup")
     @ApiOperation(value = "회원가입", notes = "사용자의 정보를 입력 받고 'success'면 회원가입 or 'fail이면 에러메세지", response = String.class)
