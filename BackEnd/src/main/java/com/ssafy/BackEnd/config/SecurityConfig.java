7c5ffe7637b3df7edb46a9e94c75b92b12357f00
SSAFY <kys980130@naver.com>
2022-02-14 17:47:55 +0900

    modify securityconfig

--- BackEnd/src/main/java/com/ssafy/BackEnd/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/ssafy/BackEnd/config/SecurityConfig.java
@@ -1,89 +1,89 @@
-//package com.ssafy.BackEnd.config;
+package com.ssafy.BackEnd.config;
+
+import com.ssafy.BackEnd.filter.JwtAuthenticationFilter;
+//import com.ssafy.BackEnd.filter.JwtRequestFilter;
+import com.ssafy.BackEnd.service.JwtServiceImpl;
+//import com.ssafy.BackEnd.service.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtServiceImpl jwtService;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+        web.ignoring().antMatchers("/swagger-resources/**",
+                "/swagger-ui.html", "/webjars/**", "/swagger/**", "/signin", "/signup", "/chat/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        System.out.println("여기니?");
+        http.httpBasic().disable();
+        //http.csrf().disable().formLogin().loginPage("/aaaaa"); // 페이지를 못가게 하는
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/team/**").permitAll() //.hasRole("USER")
+                .antMatchers("/dashboard/**").permitAll()
+                .antMatchers("/signin").permitAll()
+                .antMatchers("/signup").permitAll()
+                .antMatchers("/profile").permitAll()
+                .antMatchers("/chat/**").permitAll()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+
+                //.antMatchers("/*").permitAll();
+
+//                .antMatchers("/profile/*").permitAll();
+
+        //        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+//        filter.setEncoding("UTF-8");
+//        filter.setForceEncoding(true);
 //
-//import com.ssafy.BackEnd.filter.JwtAuthenticationFilter;
-////import com.ssafy.BackEnd.filter.JwtRequestFilter;
-//import com.ssafy.BackEnd.service.JwtServiceImpl;
-////import com.ssafy.BackEnd.service.JwtTokenProvider;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-////import org.springframework.web.filter.CharacterEncodingFilter;
 //
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//        http.csrf().disable().formLogin(); // 페이지를 못가게 하는
 //
-//    private final JwtServiceImpl jwtService;
-//
-//    @Bean
-//    @Override
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//    @Override
-//    public void configure(WebSecurity web) throws Exception
-//    {
-//        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
-//        web.ignoring().antMatchers("/swagger-resources/**",
-//                "/swagger-ui.html", "/webjars/**", "/swagger/**", "/signin", "/signup", "/chat/**");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        System.out.println("여기니?");
 //        http.httpBasic().disable();
-//        //http.csrf().disable().formLogin().loginPage("/aaaaa"); // 페이지를 못가게 하는
+//        http.csrf()
+//                .disable()
+//                .headers()
+//                .frameOptions()
+//                .disable();
 //
-//        http.csrf().disable()
-//                .authorizeRequests()
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.authorizeRequests()
 //                .antMatchers("/admin/**").hasRole("ADMIN")
-//                .antMatchers("/team/**").permitAll() //.hasRole("USER")
-//                .antMatchers("/dashboard/**").permitAll()
-//                .antMatchers("/signin").permitAll()
-//                .antMatchers("/signup").permitAll()
-//                .antMatchers("/profile").permitAll()
-//                .antMatchers("/chat/**").permitAll()
+//                .antMatchers("/team/**").hasRole("USER")
+//                .antMatchers("/signin/**").permitAll()
+//                .antMatchers("/signup/**").permitAll()
+////                .antMatchers("/signin").permitAll()
+////                .antMatchers("/signup").permitAll()
+////                .antMatchers("/team/*").hasRole("USER")
 //                .and()
 //                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
-//
-//                //.antMatchers("/*").permitAll();
-//
-////                .antMatchers("/profile/*").permitAll();
-//
-//        //        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-////        filter.setEncoding("UTF-8");
-////        filter.setForceEncoding(true);
-////
-////
-////        http.csrf().disable().formLogin(); // 페이지를 못가게 하는
-////
-////        http.httpBasic().disable();
-////        http.csrf()
-////                .disable()
-////                .headers()
-////                .frameOptions()
-////                .disable();
-////
-////        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-////        http.authorizeRequests()
-////                .antMatchers("/admin/**").hasRole("ADMIN")
-////                .antMatchers("/team/**").hasRole("USER")
-////                .antMatchers("/signin/**").permitAll()
-////                .antMatchers("/signup/**").permitAll()
-//////                .antMatchers("/signin").permitAll()
-//////                .antMatchers("/signup").permitAll()
-//////                .antMatchers("/team/*").hasRole("USER")
-////                .and()
-////                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
-//
-//    }
-//}
+
+    }
+}
--- BackEnd/src/main/java/com/ssafy/BackEnd/config/SecurityConfig1.java
+++ BackEnd/src/main/java/com/ssafy/BackEnd/config/SecurityConfig1.java
@@ -1,115 +1,115 @@
-package com.ssafy.BackEnd.config;
-
-//import com.ssafy.BackEnd.filter.JwtAuthenticationFilter;
-//import com.ssafy.BackEnd.filter.JwtRequestFilter;
-import com.ssafy.BackEnd.service.JwtServiceImpl;
-//import com.ssafy.BackEnd.service.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.filter.CharacterEncodingFilter;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig1 extends WebSecurityConfigurerAdapter {
-
-    private final JwtServiceImpl jwtService;
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception
-    {
-        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
-        web.ignoring().antMatchers("/swagger-resources/**",
-                "/swagger-ui.html", "/webjars/**", "/swagger/**");
-    }
-
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        System.out.println("여기니?");
-        http.httpBasic().disable();
-        //http.csrf().disable().formLogin().loginPage("/aaaaa"); // 페이지를 못가게 하는
-
-        http.csrf().disable()
-                .formLogin()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/team/**").hasRole("USER")
-                .antMatchers("/dashboard/**").permitAll()
-                .antMatchers("/signin").permitAll()
-                .antMatchers("/signup").permitAll()
-                .antMatchers("/chat/**").hasRole("USER");
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
-//                .authorizeRequests();
-
-                //.antMatchers("/*").permitAll();
-
-//                .antMatchers("/profile/*").permitAll();
-
-        //        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//        filter.setEncoding("UTF-8");
-//        filter.setForceEncoding(true);
+//package com.ssafy.BackEnd.config;
+//
+////import com.ssafy.BackEnd.filter.JwtAuthenticationFilter;
+////import com.ssafy.BackEnd.filter.JwtRequestFilter;
+//import com.ssafy.BackEnd.service.JwtServiceImpl;
+////import com.ssafy.BackEnd.service.JwtTokenProvider;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+////import org.springframework.web.filter.CharacterEncodingFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig1 extends WebSecurityConfigurerAdapter {
+//
+//    private final JwtServiceImpl jwtService;
+//
+//    @Bean
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Override
+//    public void configure(WebSecurity web) throws Exception
+//    {
+//        // static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 )
+//        web.ignoring().antMatchers("/swagger-resources/**",
+//                "/swagger-ui.html", "/webjars/**", "/swagger/**");
+//    }
 //
 //
-//        http.csrf().disable().formLogin(); // 페이지를 못가게 하는
 //
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        System.out.println("여기니?");
 //        http.httpBasic().disable();
-//        http.csrf()
-//                .disable()
-//                .headers()
-//                .frameOptions()
-//                .disable();
+//        //http.csrf().disable().formLogin().loginPage("/aaaaa"); // 페이지를 못가게 하는
 //
-//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.authorizeRequests()
+//        http.csrf().disable()
+//                .formLogin()
+//                .and()
+//                .authorizeRequests()
 //                .antMatchers("/admin/**").hasRole("ADMIN")
 //                .antMatchers("/team/**").hasRole("USER")
-//                .antMatchers("/signin/**").permitAll()
-//                .antMatchers("/signup/**").permitAll()
-////                .antMatchers("/signin").permitAll()
-////                .antMatchers("/signup").permitAll()
-////                .antMatchers("/team/*").hasRole("USER")
+//                .antMatchers("/dashboard/**").permitAll()
+//                .antMatchers("/signin").permitAll()
+//                .antMatchers("/signup").permitAll()
+//                .antMatchers("/chat/**").hasRole("USER");
+////                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
+////                .authorizeRequests();
+//
+//                //.antMatchers("/*").permitAll();
+//
+////                .antMatchers("/profile/*").permitAll();
+//
+//        //        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+////        filter.setEncoding("UTF-8");
+////        filter.setForceEncoding(true);
+////
+////
+////        http.csrf().disable().formLogin(); // 페이지를 못가게 하는
+////
+////        http.httpBasic().disable();
+////        http.csrf()
+////                .disable()
+////                .headers()
+////                .frameOptions()
+////                .disable();
+////
+////        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+////        http.authorizeRequests()
+////                .antMatchers("/admin/**").hasRole("ADMIN")
+////                .antMatchers("/team/**").hasRole("USER")
+////                .antMatchers("/signin/**").permitAll()
+////                .antMatchers("/signup/**").permitAll()
+//////                .antMatchers("/signin").permitAll()
+//////                .antMatchers("/signup").permitAll()
+//////                .antMatchers("/team/*").hasRole("USER")
+////                .and()
+////                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+//
+//    }
+//
+//
+//        /**
+//         * 테스트를 위해 In-Memory에 계정을 임의로 생성한다.
+//         * 서비스에 사용시에는 DB데이터를 이용하도록 수정이 필요하다.
+//         */
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("eunseo")
+//                .password("{noop}1234")
+//                .roles("USER")
 //                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
-
-    }
-
-
-        /**
-         * 테스트를 위해 In-Memory에 계정을 임의로 생성한다.
-         * 서비스에 사용시에는 DB데이터를 이용하도록 수정이 필요하다.
-         */
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("eunseo")
-                .password("{noop}1234")
-                .roles("USER")
-                .and()
-                .withUser("eunseo1")
-                .password("{noop}1234")
-                .roles("USER")
-                .and()
-                .withUser("guest")
-                .password("{noop}1234")
-                .roles("GUEST");
-
-    }
-}
+//                .withUser("eunseo1")
+//                .password("{noop}1234")
+//                .roles("USER")
+//                .and()
+//                .withUser("guest")
+//                .password("{noop}1234")
+//                .roles("GUEST");
+//
+//    }
+//}
