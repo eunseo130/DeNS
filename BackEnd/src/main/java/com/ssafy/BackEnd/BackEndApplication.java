027d7f6a671b7287cf9f12bdeecb5302d1b7f302
SSAFY <kys980130@naver.com>
2022-02-16 15:41:37 +0900

    modify chat

--- BackEnd/src/main/java/com/ssafy/BackEnd/BackEndApplication.java
+++ BackEnd/src/main/java/com/ssafy/BackEnd/BackEndApplication.java
@@ -8,11 +8,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableWebSocketMessageBroker
 public class BackEndApplication {
 
 	public static void main(String[] args) {
