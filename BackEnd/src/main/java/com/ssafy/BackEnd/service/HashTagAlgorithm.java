8dd37671abbfb0ec48db9094279f99a5c4b9e61f
JunWoo Kim <k1mjunooo@naver.com>
2022-02-17 15:32:10 +0900

    modify hashtag Algo

--- BackEnd/src/main/java/com/ssafy/BackEnd/service/HashTagAlgorithm.java
+++ BackEnd/src/main/java/com/ssafy/BackEnd/service/HashTagAlgorithm.java
@@ -1,22 +1,18 @@
 package com.ssafy.BackEnd.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HashTagAlgorithm {
-
     public static List<String> strList(String content){
+
         List<String> strlist = new ArrayList<>();
-        String[] strArr = content.split(" ");
+        String[] strArr = content.split("#| ");
         for (String s : strArr){
-//            if(s.equals(" ")) continue;
-//            String key = s;
-//            strlist.add(key);
-            if(s.charAt(0) == '#') {
-                String key = s.substring(1);
-                strlist.add(key);
-            }
-//>>>>>>> d5e6f18b48cd62a9b37d44dde9ce607ece3b3136
+            if(s.equals("")) continue;
+            System.out.println("해시태그 #"+s);
+            strlist.add(s);
         }
         return strlist;
     }
