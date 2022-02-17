5a498fc7f7e7df49d5d005bf376ff49ef59838de
yejin <jasmin960427@naver.com>
2022-02-17 11:07:15 +0900

    modify port num

--- BackEnd/src/main/java/com/ssafy/BackEnd/service/HashTagAlgorithm.java
+++ BackEnd/src/main/java/com/ssafy/BackEnd/service/HashTagAlgorithm.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class HashTagAlgorithm {
 
-    public static List<String> strList(String content){
+    public static List<String> strList(String content) {
         List<String> strlist = new ArrayList<>();
         String[] strArr = content.split(" ");
         for (String s : strArr){
